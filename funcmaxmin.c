f28a57a2a24a23bbf273cc847fdf5a704aa0f9df
Manav Shree <124267787+manavshree@users.noreply.github.com>
2024-10-05 17:06:08 +0530

    Add files via upload

---
+++ funcmaxmin.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int maxmin(int array[])
+{
+    
+}
+int main()
+{
+    int n;
+    printf("enter the number of array:\n");
+    scanf("%d",&n);
+    int array[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",array[i]);
+    }
+    return 0;
+}
