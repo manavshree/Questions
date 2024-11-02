880ed26676d1ef24d7d9f62fbbdad83dde9efcba
Manav Shree <124267787+manavshree@users.noreply.github.com>
2024-11-02 23:16:47 +0530

    Add files via upload

---
+++ alpgf.c
@@ -0,0 +1,18 @@
+vid Meyt tin! bLI, int cl], int al 2 int p, int { 
+int i=0, j=0 K=0 
+ele 
+whide (i<p) 
+a [tt] blit+): 
+stide j<e) 
+a LkttJ= 6Lit 
+a[k+t cjtil 
+alktt cjt 
+vo,d Mege frt (int all, int nf 
+it mid = n/2; 
+int blid 
+int cCn - m 
+fo Lint i= 0 i<md, i+ +) 
+) lint i= midi ikn it+) 
+cLi-nils a lid; 
+Mergehnt Cc n-mid)s 
+Merge l b, Gia, mid, n- mid)j
