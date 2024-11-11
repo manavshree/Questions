6d77ff9a52cba1ff02275c758943427137a17423
Manav Shree <124267787+manavshree@users.noreply.github.com>
2024-11-11 15:37:05 +0530

    Add files via upload

---
+++ 2_deletion.c
@@ -0,0 +1,13 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int arr[100]={1,2,3,4,5,6};
+//     int size,pos,value;
+//     printf("enter the position:\n");
+//     scanf("%d",&pos);
+//     printf("enter the value:\n");
+//     scanf("%d",&value);
+//     size=5;
+//     // for(int i=)
+//     return 0;
+// }
